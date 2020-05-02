8996146609a68925bc5ad5ebe50dd9ba1c59d6f3
zfy <rachmanino@yahoo.com>
2020-05-02 23:33:05 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,24 +13,22 @@ dirs=(/home/travis/build/fy-travis-projects/*)
 name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
 echo $name
 
-# collect 3rd party jars into one folder
-# cd $HOME 
-# mkdir lib
-# cd $HOME/.m2/repository
-# pwd 
-# ls -al
-# find . -name '*.jar' -exec mv {} $HOME/lib \;
+collect 3rd party jars into one folder
+cd $HOME 
+mkdir lib
+cd $HOME/.m2/repository
+pwd 
+ls -al
+find . -name '*.jar' -exec mv {} $HOME/lib \;
 
-# collect build artifact jars  into one folder
-# cd $HOME
-# mkdir project
-# cd $HOME/build/fy-travis-projects/$name
-# find . -name '*.jar' -exec mv {} $HOME/project \;
-# cd $HOME
-
+collect build artifact jars  into one folder
+cd $HOME
+mkdir project
 cd $HOME/build/fy-travis-projects/$name
+find . -name '*.jar' -exec mv {} $HOME/project \;
+cd $HOME
 pwd
 ls -al
 
-# rsync -W -rav -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 lib travis@35.236.128.26:/home/travis/projects/$name/
-# rsync -W -rav -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 project travis@35.236.128.26:/home/travis/projects/$name/
+rsync -W -rav -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 lib travis@35.236.128.26:/home/travis/projects/$name/
+rsync -W -rav -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 project travis@35.236.128.26:/home/travis/projects/$name/
